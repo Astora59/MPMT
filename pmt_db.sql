1bcdad5cb40aeb691c037c007ac48277aa34ed45
Yamin <yaminmoulay12@gmail.com>
2025-08-29 18:40:58 +0200

    Completed SQL script

--- pmt_db.sql
+++ pmt_db.sql
@@ -23,7 +23,8 @@ CREATE TABLE project (
     project_name VARCHAR(50) NOT NULL,
     project_description TEXT,
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    users_email VARCHAR(50) NOT NULL REFERENCES "users"(users_email) ON DELETE CASCADE
+    users_email VARCHAR(50) NOT NULL REFERENCES "users"(users_email) ON DELETE CASCADE,
+    project_admin UUID NOT NULL REFERENCES users(users_id) ON DELETE CASCADE
 );
 
 
@@ -36,7 +37,8 @@ CREATE TABLE role (
     role_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     role_name VARCHAR(14) NOT NULL CHECK (role_name IN ('admin', 'member', 'observer')),
     users_id UUID NOT NULL REFERENCES "users"(users_id) ON DELETE CASCADE,
-    project_id UUID NOT NULL REFERENCES "project"(project_id) ON DELETE CASCADE
+    project_id UUID NOT NULL REFERENCES "project"(project_id) ON DELETE CASCADE,
+    CONSTRAINT unique_user_project UNIQUE (users_id, project_id)
     
 );
 
@@ -94,10 +96,34 @@ CREATE TABLE notification (
 INSERT INTO "users" (usersname, users_email, password_hash) VALUES ('dave_dave', 'dave_dave@gmail.com', 'hashed_password_987');
 INSERT INTO "users" (usersname, users_email, password_hash) VALUES ('bella_ciao', 'bella_ciao@gmail.com', 'hashed_password_654');
 
-INSERT INTO "project" (project_name, project_description, users_email) VALUES ('Project Delta', 'This is the third project.', 'dave_dave@gmail.com');
-INSERT INTO "project" (project_name, project_description, users_email) VALUES ('Project Sigma', 'This is the fourth project.', 'bella_ciao@gmail.com');
+INSERT INTO project (project_name, project_description, users_email, project_admin)
+VALUES (
+    'Project Test trigger',
+    'Projet pour test un trigger',
+    'bella_ciao@gmail.com',  
+    (SELECT users_id FROM users WHERE users_email='bella_ciao@gmail.com')
+);
 
 INSERT INTO "role" (role_name, users_id, project_id) VALUES ('observer', (SELECT users_id FROM "users" WHERE usersname='dave_dave'), (SELECT project_id FROM "project" WHERE project_name='Project Delta'));
 INSERT INTO "role" (role_name, users_id, project_id) VALUES ('member', (SELECT users_id FROM "users" WHERE usersname='bella_ciao'), (SELECT project_id FROM "project" WHERE project_name='Project Sigma'));
 
 INSERT INTO "task" (task_name, task_title, task_description, task_deadline, task_status, task_priority, project_id) VALUES ('Task 1', 'Initial Setup', 'Set up the initial project structure.', '2023-12-01 10:00:00', 'pending', 'high', (SELECT project_id FROM "project" WHERE project_name='Project Delta'));
+
+
+
+-- trigger
+CREATE OR REPLACE FUNCTION add_admin_role()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO role (role_name, users_id, project_id)
+    VALUES ('admin', NEW.project_admin, NEW.project_id);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_add_admin_role
+AFTER INSERT ON project
+FOR EACH ROW
+EXECUTE FUNCTION add_admin_role();
+
+
