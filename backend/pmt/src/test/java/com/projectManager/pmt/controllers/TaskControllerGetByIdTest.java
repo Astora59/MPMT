30b0d132876a47753bbd66f0507b51c8a21e6091
Yamin <yaminmoulay12@gmail.com>
2026-04-16 11:12:14 +0200

    modified tests

--- backend/pmt/src/test/java/com/projectManager/pmt/controllers/TaskControllerGetByIdTest.java
+++ backend/pmt/src/test/java/com/projectManager/pmt/controllers/TaskControllerGetByIdTest.java
@@ -4,6 +4,7 @@ import com.projectManager.pmt.models.Task;
 import com.projectManager.pmt.services.TaskService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(TaskController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class TaskControllerGetByIdTest {
 
     @Autowired
