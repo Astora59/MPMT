1d9c67af0fbe598935bda7e221d9f1754c4821de
Yamin <yaminmoulay12@gmail.com>
2026-04-16 10:41:01 +0200

    modified rendering of project route

--- frontend/pmt_front/src/app/app.routes.server.ts
+++ frontend/pmt_front/src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: 'project/:id',
     renderMode: RenderMode.Server
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender
   }
 ];
